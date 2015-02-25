b473d35ed2fcb88931b22a01d35a75b48282552a
Ibrar Ahmed <ibrar.ahmad@gmail.com>
2015-02-26 02:03:46 +0500

    Error - (#23) Test case update

--- sql/mongo_fdw.sql
+++ sql/mongo_fdw.sql
@@ -1,41 +1,41 @@
 CREATE SERVER mongo_server FOREIGN DATA WRAPPER mongo_fdw OPTIONS (address '127.0.0.1', port '27017');
 \! mongoimport --db mongo_fdw_regress --collection countries --jsonArray --drop --quiet < data/mongo_fixture.json
 
-CREATE USER MAPPING FOR postgres SERVER mongo_server;
+CREATE USER MAPPING FOR ibrar SERVER mongo_server;
 
 CREATE FOREIGN TABLE department(_id NAME, department_id int, department_name text) SERVER mongo_server OPTIONS(database 'testdb', collection 'department');
 CREATE FOREIGN TABLE employee(_id NAME, emp_id int, emp_name text, emp_dept_id int) SERVER mongo_server OPTIONS(database 'testdb', collection 'employee');
 
-INSERT INTO department VALUES(0, generate_series(1,100), 'dept - ' || generate_series(1,100));
-INSERT INTO employee VALUES(0, generate_series(1,100), 'emp - ' || generate_series(1,100), generate_series(1,100));
+INSERT INTO department VALUES(0, generate_series(1,10), 'dept - ' || generate_series(1,10));
+INSERT INTO employee VALUES(0, generate_series(1,100), 'emp - ' || generate_series(1,100), generate_series(1,10));
 
 SELECT count(*) FROM department;
 SELECT count(*) FROM employee;
 
-EXPLAIN (COSTS FALSE) SELECT emp_id , emp_name , emp_dept_id, department_id , department_name  FROM department d, employee e WHERE d.department_id = e.emp_dept_id LIMIT 10;
+EXPLAIN (COSTS FALSE) SELECT emp_id , emp_name , emp_dept_id, department_id , department_name  FROM department d, employee e WHERE d.department_id = e.emp_dept_id ORDER by emp_id;
 
-EXPLAIN (COSTS FALSE) SELECT emp_id , emp_name , emp_dept_id, department_id , department_name FROM department d, employee e WHERE d.department_id IN (SELECT department_id FROM department) LIMIT 10;
+EXPLAIN (COSTS FALSE) SELECT emp_id , emp_name , emp_dept_id, department_id , department_name FROM department d, employee e WHERE d.department_id IN (SELECT department_id FROM department) ORDER by emp_id;
 
-SELECT emp_id , emp_name , emp_dept_id, department_id , department_name FROM department d, employee e WHERE d.department_id = e.emp_dept_id LIMIT 10;
-SELECT emp_id , emp_name , emp_dept_id, department_id , department_name FROM department d, employee e WHERE d.department_id IN (SELECT department_id FROM department) LIMIT 10;
+SELECT emp_id , emp_name , emp_dept_id, department_id , department_name FROM department d, employee e WHERE d.department_id = e.emp_dept_id ORDER by emp_id;
+SELECT emp_id , emp_name , emp_dept_id, department_id , department_name FROM department d, employee e WHERE d.department_id IN (SELECT department_id FROM department) ORDER by emp_id;
 
 DELETE FROM employee WHERE emp_id = 10;
 
 UPDATE employee SET emp_name = 'Updated emp' WHERE emp_id = 20;
 SELECT emp_id, emp_name FROM employee WHERE emp_name like 'Updated emp';
 
-SELECT emp_id , emp_name , emp_dept_id FROM employee LIMIT 10;
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id IN (1);
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id IN (1,3,4,5);
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id IN (10000,1000);
+SELECT emp_id , emp_name , emp_dept_id FROM employee ORDER by emp_id LIMIT 10;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id IN (1) ORDER by emp_id;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id IN (1,3,4,5) ORDER by emp_id;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id IN (10000,1000) ORDER by emp_id;
 
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id NOT IN (1) LIMIT 5;
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id NOT IN (1,3,4,5) LIMIT 5;
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id NOT IN (10000,1000) LIMIT 5;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id NOT IN (1)  ORDER by emp_id LIMIT 5;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id NOT IN (1,3,4,5) ORDER by emp_id LIMIT 5;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id NOT IN (10000,1000) ORDER by emp_id LIMIT 5;
 
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id NOT IN (SELECT emp_id FROM employee WHERE emp_id IN (1,10));
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_name NOT IN ('emp - 1', 'emp - 2') LIMIT 5;
-SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_name NOT IN ('emp - 10') LIMIT 5;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_id NOT IN (SELECT emp_id FROM employee WHERE emp_id IN (1,10)) ORDER by emp_id;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_name NOT IN ('emp - 1', 'emp - 2') ORDER by emp_id LIMIT 5;
+SELECT emp_id , emp_name , emp_dept_id FROM employee WHERE emp_name NOT IN ('emp - 10') ORDER by emp_id LIMIT 5;
 
 DELETE FROM employee;
 DELETE FROM department;
