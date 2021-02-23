citusdata/mongo_fdw

commit fce56c68aaa03f6aa512f2466b25bdf92a72147d

Rfactor WHERE pushdown code.
Earlier, we are finding the pushable OpExpr by calling the function
ApplicableOpExpressionList().  Change that to the postgres_fdw
way of having a mongo_is_foreign_expr() function and a
foreign_expr_walker().  This is cleaner and in-line with PostgreSQL
that can be extended easily.  This refactoring allowed is to push
down the clauses having a Var node wrapped in the RelabelType.

Also, for no reason but just for simplicity, we were re-evaluating
restriction clauses that are pushed down to MongoDB again on the
local side.  Remove that altogether which adds to some performance
improvement.

FDW-194, Vaibhav Dalvi, reviewed by Suraj Kharage, further
improvements by Jeevan Chalke.

data/mongo_test_data.js
@@ -11,6 +11,7 @@ db.dropDatabase();
 use mongo_fdw_regress
 db.test_tbl1.drop();
 db.test_tbl2.drop();
+db.test_tbl3.drop();
 db.mongo_test.drop();
 // Below queries will create and insert values in collections
 db.mongo_test.insert({a : NumberInt(0), b : "mongo_test collection"});
@@ -35,4 +36,8 @@ db.test_tbl1.insertMany([
    {c1: NumberInt(1200), c2 : "EMP12", c3 :"ADMIN", c4 :NumberInt(600) ,c5 :ISODate("1981-12-03"), c6 :950.00, c7 :NumberInt(0), c8 :NumberInt(30)  },
    {c1: NumberInt(1300), c2 : "EMP13", c3 :"FINANCE", c4 :NumberInt(400) ,c5 :ISODate("1981-12-03"), c6 :3000, c7 :NumberInt(0), c8 :NumberInt(20) },
    {c1: NumberInt(1400), c2 : "EMP14", c3 :"ADMIN", c4 :NumberInt(700) ,c5 :ISODate("1982-01-23"), c6 :1300, c7 :NumberInt(0), c8 :NumberInt(10)  },
+]);
+db.test_tbl3.insertMany([
+   {name: "dvd", marks: [23, 24]},
+   {name: "vdd", marks: [29, 31]}
 ]);

expected/pushdown.out
@@ -17,6 +17,12 @@ CREATE FOREIGN TABLE f_test_tbl1 (_id name, c1 INTEGER, c2 VARCHAR(10), c3 CHAR(
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl1');
 CREATE FOREIGN TABLE f_test_tbl2 (_id name, c1 INTEGER, c2 VARCHAR(14), c3 VARCHAR(13))
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl2');
+CREATE FOREIGN TABLE f_test_tbl3 (_id name, name TEXT, marks FLOAT ARRAY)
+  SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl3');
+-- Inserts some values in mongo_test collection.
+INSERT INTO f_mongo_test VALUES ('0', 1, 'One');
+INSERT INTO f_mongo_test VALUES ('0', 2, 'Two');
+INSERT INTO f_mongo_test VALUES ('0', 3, 'Three');
 SET datestyle TO ISO;
 -- Sample data
 SELECT c1, c2, c3, c4, c5, c6, c7, c8 FROM f_test_tbl1 ORDER BY c1;
@@ -73,9 +79,8 @@ SELECT c1, c2, c6 FROM f_test_tbl1 e
    Sort Key: e.c1
    ->  Foreign Scan on public.f_test_tbl1 e
          Output: c1, c2, c6
-         Filter: (e.c6 > '3000'::numeric)
          Foreign Namespace: mongo_fdw_regress.test_tbl1
-(7 rows)
+(6 rows)
 
 SELECT c1, c2, c6 FROM f_test_tbl1 e
   WHERE c6 > 3000
@@ -96,9 +101,8 @@ SELECT c1, c2, c6, c8 FROM f_test_tbl1 e
    Sort Key: e.c1
    ->  Foreign Scan on public.f_test_tbl1 e
          Output: c1, c2, c6, c8
-         Filter: (e.c6 = '1500'::numeric)
          Foreign Namespace: mongo_fdw_regress.test_tbl1
-(7 rows)
+(6 rows)
 
 SELECT c1, c2, c6, c8 FROM f_test_tbl1 e
   WHERE c6 = 1500
@@ -112,16 +116,15 @@ EXPLAIN (VERBOSE, COSTS FALSE)
 SELECT c1, c2, c6, c8 FROM f_test_tbl1 e
   WHERE c6 BETWEEN 1000 AND 4000
   ORDER BY c1;
-                                QUERY PLAN                                 
----------------------------------------------------------------------------
+                       QUERY PLAN                       
+--------------------------------------------------------
  Sort
    Output: c1, c2, c6, c8
    Sort Key: e.c1
    ->  Foreign Scan on public.f_test_tbl1 e
          Output: c1, c2, c6, c8
-         Filter: ((e.c6 >= '1000'::numeric) AND (e.c6 <= '4000'::numeric))
          Foreign Namespace: mongo_fdw_regress.test_tbl1
-(7 rows)
+(6 rows)
 
 SELECT c1, c2, c6, c8 FROM f_test_tbl1 e
   WHERE c6 BETWEEN 1000 AND 4000
@@ -187,9 +190,8 @@ SELECT c1, c2, c5 FROM f_test_tbl1 e
    Sort Key: e.c1
    ->  Foreign Scan on public.f_test_tbl1 e
          Output: c1, c2, c5
-         Filter: (e.c5 <= '1980-12-17'::date)
          Foreign Namespace: mongo_fdw_regress.test_tbl1
-(7 rows)
+(6 rows)
 
 SELECT c1, c2, c5 FROM f_test_tbl1 e
   WHERE c5 <= '1980-12-17'
@@ -272,10 +274,197 @@ SELECT c1, c2, c6, c8 FROM f_test_tbl1 e
  700 | EMP7 | 2450.34 | 10
 (3 rows)
 
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT a FROM f_mongo_test
+  WHERE a%2 = 1
+  ORDER BY a;
+                       QUERY PLAN                        
+---------------------------------------------------------
+ Sort
+   Output: a
+   Sort Key: f_mongo_test.a
+   ->  Foreign Scan on public.f_mongo_test
+         Output: a
+         Filter: ((f_mongo_test.a % 2) = 1)
+         Foreign Namespace: mongo_fdw_regress.mongo_test
+(7 rows)
+
+SELECT a FROM f_mongo_test
+  WHERE a%2 = 1
+  ORDER BY a;
+ a 
+---
+ 1
+ 3
+(2 rows)
+
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT a, b FROM f_mongo_test
+  WHERE a >= 1 AND b LIKE '%O%'
+  ORDER BY a;
+                       QUERY PLAN                        
+---------------------------------------------------------
+ Sort
+   Output: a, b
+   Sort Key: f_mongo_test.a
+   ->  Foreign Scan on public.f_mongo_test
+         Output: a, b
+         Filter: ((f_mongo_test.b)::text ~~ '%O%'::text)
+         Foreign Namespace: mongo_fdw_regress.mongo_test
+(7 rows)
+
+SELECT a, b FROM f_mongo_test
+  WHERE a >= 1 AND b LIKE '%O%'
+  ORDER BY a;
+ a |  b  
+---+-----
+ 1 | One
+(1 row)
+
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2, c5 FROM f_test_tbl1 e
+  WHERE c5 <= '1980-12-17' AND c2 IN ('EMP1', 'EMP5', 'EMP10') AND c1 = 100
+  ORDER BY c1;
+                          QUERY PLAN                          
+--------------------------------------------------------------
+ Foreign Scan on public.f_test_tbl1 e
+   Output: c1, c2, c5
+   Filter: ((e.c2)::text = ANY ('{EMP1,EMP5,EMP10}'::text[]))
+   Foreign Namespace: mongo_fdw_regress.test_tbl1
+(4 rows)
+
+SELECT c1, c2, c5 FROM f_test_tbl1 e
+  WHERE c5 <= '1980-12-17' AND c2 IN ('EMP1', 'EMP5', 'EMP10') AND c1 = 100
+  ORDER BY c1;
+ c1  |  c2  |     c5     
+-----+------+------------
+ 100 | EMP1 | 1980-12-17
+(1 row)
+
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE c2 = 'EMP10';
+                    QUERY PLAN                    
+--------------------------------------------------
+ Foreign Scan on public.f_test_tbl1
+   Output: c1, c2
+   Foreign Namespace: mongo_fdw_regress.test_tbl1
+(3 rows)
+
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE c2 = 'EMP10';
+  c1  |  c2   
+------+-------
+ 1000 | EMP10
+(1 row)
+
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE c2 < 'EMP10';
+                    QUERY PLAN                    
+--------------------------------------------------
+ Foreign Scan on public.f_test_tbl1
+   Output: c1, c2
+   Foreign Namespace: mongo_fdw_regress.test_tbl1
+(3 rows)
+
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE c2 < 'EMP10';
+ c1  |  c2  
+-----+------
+ 100 | EMP1
+(1 row)
+
+-- Should not push down if two columns of same table is
+-- involved in single WHERE clause operator expression.
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c6 FROM f_test_tbl1
+  WHERE c1 = c6 AND c1 = 1100
+  ORDER BY c1;
+                       QUERY PLAN                       
+--------------------------------------------------------
+ Foreign Scan on public.f_test_tbl1
+   Output: c1, c6
+   Filter: ((f_test_tbl1.c1)::numeric = f_test_tbl1.c6)
+   Foreign Namespace: mongo_fdw_regress.test_tbl1
+(4 rows)
+
+SELECT c1, c6 FROM f_test_tbl1
+  WHERE c1 = c6 AND c1 = 1100
+  ORDER BY c1;
+  c1  |  c6  
+------+------
+ 1100 | 1100
+(1 row)
+
+-- Nested operator expression in WHERE clause. Shouldn't push down.
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE (c1 > 1000) > FALSE;
+                    QUERY PLAN                    
+--------------------------------------------------
+ Foreign Scan on public.f_test_tbl1
+   Output: c1, c2
+   Filter: ((f_test_tbl1.c1 > 1000) > false)
+   Foreign Namespace: mongo_fdw_regress.test_tbl1
+(4 rows)
+
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE (c1 > 1000) > FALSE;
+  c1  |  c2   
+------+-------
+ 1100 | EMP11
+ 1200 | EMP12
+ 1300 | EMP13
+ 1400 | EMP14
+(4 rows)
+
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE (c1 > 1000) > 0::BOOLEAN;
+                    QUERY PLAN                    
+--------------------------------------------------
+ Foreign Scan on public.f_test_tbl1
+   Output: c1, c2
+   Filter: ((f_test_tbl1.c1 > 1000) > false)
+   Foreign Namespace: mongo_fdw_regress.test_tbl1
+(4 rows)
+
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE (c1 > 1000) > 0::BOOLEAN;
+  c1  |  c2   
+------+-------
+ 1100 | EMP11
+ 1200 | EMP12
+ 1300 | EMP13
+ 1400 | EMP14
+(4 rows)
+
+-- Shouldn't push down operators where the constant is an array.
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT name, marks FROM f_test_tbl3
+  WHERE marks = ARRAY[23::FLOAT, 24::FLOAT]
+  ORDER BY name;
+                             QUERY PLAN                              
+---------------------------------------------------------------------
+ Sort
+   Output: name, marks
+   Sort Key: f_test_tbl3.name
+   ->  Foreign Scan on public.f_test_tbl3
+         Output: name, marks
+         Filter: (f_test_tbl3.marks = '{23,24}'::double precision[])
+         Foreign Namespace: mongo_fdw_regress.test_tbl3
+(7 rows)
+
+SELECT name, marks FROM f_test_tbl3
+  WHERE marks = ARRAY[23::FLOAT, 24::FLOAT]
+  ORDER BY name;
+ name |  marks  
+------+---------
+ dvd  | {23,24}
+(1 row)
+
 -- Pushdown in prepared statement.
-INSERT INTO f_mongo_test VALUES ('0', 1, 'One');
-INSERT INTO f_mongo_test VALUES ('0', 2, 'Two');
-INSERT INTO f_mongo_test VALUES ('0', 3, 'Three');
 PREPARE pre_stmt_f_mongo_test(int) AS
   SELECT b FROM f_mongo_test WHERE a = $1 ORDER BY b;
 EXPLAIN (VERBOSE, COSTS FALSE)
@@ -287,9 +476,8 @@ EXECUTE pre_stmt_f_mongo_test(1);
    Sort Key: f_mongo_test.b
    ->  Foreign Scan on public.f_mongo_test
          Output: b
-         Filter: (f_mongo_test.a = 1)
          Foreign Namespace: mongo_fdw_regress.mongo_test
-(7 rows)
+(6 rows)
 
 EXECUTE pre_stmt_f_mongo_test(1);
   b  
@@ -306,9 +494,8 @@ EXECUTE pre_stmt_f_mongo_test(2);
    Sort Key: f_mongo_test.b
    ->  Foreign Scan on public.f_mongo_test
          Output: b
-         Filter: (f_mongo_test.a = 2)
          Foreign Namespace: mongo_fdw_regress.mongo_test
-(7 rows)
+(6 rows)
 
 EXECUTE pre_stmt_f_mongo_test(2);
   b  
@@ -321,6 +508,7 @@ DELETE FROM f_mongo_test WHERE a != 0;
 DROP FOREIGN TABLE f_mongo_test;
 DROP FOREIGN TABLE f_test_tbl1;
 DROP FOREIGN TABLE f_test_tbl2;
+DROP FOREIGN TABLE f_test_tbl3;
 DROP USER MAPPING FOR public SERVER mongo_server;
 DROP SERVER mongo_server;
 DROP EXTENSION mongo_fdw;

mongo_fdw.c
@@ -247,6 +247,29 @@ MongoGetForeignRelSize(PlannerInfo *root,
 					   Oid foreigntableid)
 {
 	double		documentCount = ForeignTableDocumentCount(foreigntableid);
+	MongoFdwRelationInfo *fpinfo;
+	ListCell   *lc;
+
+	/*
+	 * We use MongoFdwRelationInfo to pass various information to subsequent
+	 * functions.
+	 */
+	fpinfo = (MongoFdwRelationInfo *) palloc0(sizeof(MongoFdwRelationInfo));
+	baserel->fdw_private = (void *) fpinfo;
+
+	/*
+	 * Identify which baserestrictinfo clauses can be sent to the remote
+	 * server and which can't.
+	 */
+	foreach(lc, baserel->baserestrictinfo)
+	{
+		RestrictInfo *ri = (RestrictInfo *) lfirst(lc);
+
+		if (mongo_is_foreign_expr(root, baserel, ri->clause))
+			fpinfo->remote_conds = lappend(fpinfo->remote_conds, ri);
+		else
+			fpinfo->local_conds = lappend(fpinfo->local_conds, ri);
+	}
 
 	if (documentCount > 0.0)
 	{
@@ -297,6 +320,7 @@ MongoGetForeignPaths(PlannerInfo *root,
 	Cost		startupCost = 0.0;
 	Cost		totalCost = 0.0;
 	Path	   *foreignPath;
+	MongoFdwRelationInfo *fpinfo = (MongoFdwRelationInfo *) baserel->fdw_private;
 
 	documentCount = ForeignTableDocumentCount(foreigntableid);
 
@@ -306,7 +330,7 @@ MongoGetForeignPaths(PlannerInfo *root,
 		 * We estimate the number of rows returned after restriction
 		 * qualifiers are applied by MongoDB.
 		 */
-		opExpressionList = ApplicableOpExpressionList(baserel);
+		opExpressionList = fpinfo->remote_conds;
 		documentSelectivity = clauselist_selectivity(root, opExpressionList,
 													 0, JOIN_INNER, NULL);
 		inputRowCount = clamp_row_est(documentCount * documentSelectivity);
@@ -376,13 +400,15 @@ MongoGetForeignPlan(PlannerInfo *root,
 					List *restrictionClauses,
 					Plan *outer_plan)
 {
+	MongoFdwRelationInfo *fpinfo = (MongoFdwRelationInfo *) foreignrel->fdw_private;
 	Index		scanRangeTableIndex = foreignrel->relid;
 	ForeignScan *foreignScan;
 	List	   *foreignPrivateList;
-	List	   *opExpressionList;
 	List	   *columnList;
 	List	   *scan_var_list;
 	ListCell   *lc;
+	List	   *local_exprs = NIL;
+	List	   *remote_exprs = NIL;
 
 #if PG_VERSION_NUM >= 90600
 	scan_var_list = pull_var_clause((Node *) foreignrel->reltarget->exprs,
@@ -416,28 +442,40 @@ MongoGetForeignPlan(PlannerInfo *root,
 	}
 
 	/*
-	 * We push down applicable restriction clauses to MongoDB, but for
-	 * simplicity we currently put all the restrictionClauses into the plan
-	 * node's qual list for the executor to re-check.  So all we have to do
-	 * here is strip RestrictInfo nodes from the clauses and ignore
-	 * pseudoconstants (which will be handled elsewhere).
+	 * Separate the restrictionClauses into those that can be executed remotely
+	 * and those that can't.  baserestrictinfo clauses that were previously
+	 * determined to be safe or unsafe are shown in fpinfo->remote_conds and
+	 * fpinfo->local_conds.  Anything else in the restrictionClauses list will
+	 * be a join clause, which we have to check for remote-safety.
 	 */
-	restrictionClauses = extract_actual_clauses(restrictionClauses, false);
+	foreach(lc, restrictionClauses)
+	{
+		RestrictInfo *rinfo = (RestrictInfo *) lfirst(lc);
 
-	/*
-	 * Find the foreign relation's clauses, which can be transformed to
-	 * equivalent MongoDB queries.
-	 */
-	opExpressionList = ApplicableOpExpressionList(foreignrel);
+		Assert(IsA(rinfo, RestrictInfo));
+
+		/* Ignore pseudoconstants, they are dealt with elsewhere */
+		if (rinfo->pseudoconstant)
+			continue;
+
+		if (list_member_ptr(fpinfo->remote_conds, rinfo))
+			remote_exprs = lappend(remote_exprs, rinfo->clause);
+		else if (list_member_ptr(fpinfo->local_conds, rinfo))
+			local_exprs = lappend(local_exprs, rinfo->clause);
+		else if (mongo_is_foreign_expr(root, foreignrel, rinfo->clause))
+			remote_exprs = lappend(remote_exprs, rinfo->clause);
+		else
+			local_exprs = lappend(local_exprs, rinfo->clause);
+	}
 
 	/* We don't need to serialize column list as lists are copiable */
 	columnList = ColumnList(foreignrel);
 
 	/* Construct foreign plan with query document and column list */
-	foreignPrivateList = list_make2(columnList, opExpressionList);
+	foreignPrivateList = list_make2(columnList, remote_exprs);
 
 	/* Create the foreign scan node */
-	foreignScan = make_foreignscan(targetList, restrictionClauses,
+	foreignScan = make_foreignscan(targetList, local_exprs,
 								   scanRangeTableIndex,
 								   NIL, /* No expressions to evaluate */
 								   foreignPrivateList

sql/pushdown.sql
@@ -20,6 +20,13 @@ CREATE FOREIGN TABLE f_test_tbl1 (_id name, c1 INTEGER, c2 VARCHAR(10), c3 CHAR(
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl1');
 CREATE FOREIGN TABLE f_test_tbl2 (_id name, c1 INTEGER, c2 VARCHAR(14), c3 VARCHAR(13))
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl2');
+CREATE FOREIGN TABLE f_test_tbl3 (_id name, name TEXT, marks FLOAT ARRAY)
+  SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl3');
+
+-- Inserts some values in mongo_test collection.
+INSERT INTO f_mongo_test VALUES ('0', 1, 'One');
+INSERT INTO f_mongo_test VALUES ('0', 2, 'Two');
+INSERT INTO f_mongo_test VALUES ('0', 3, 'Three');
 
 SET datestyle TO ISO;
 
@@ -99,11 +106,74 @@ SELECT c1, c2, c6, c8 FROM f_test_tbl1 e
   WHERE c3 LIKE 'MANA%'
   ORDER BY c1;
 
--- Pushdown in prepared statement.
-INSERT INTO f_mongo_test VALUES ('0', 1, 'One');
-INSERT INTO f_mongo_test VALUES ('0', 2, 'Two');
-INSERT INTO f_mongo_test VALUES ('0', 3, 'Three');
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT a FROM f_mongo_test
+  WHERE a%2 = 1
+  ORDER BY a;
+SELECT a FROM f_mongo_test
+  WHERE a%2 = 1
+  ORDER BY a;
 
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT a, b FROM f_mongo_test
+  WHERE a >= 1 AND b LIKE '%O%'
+  ORDER BY a;
+SELECT a, b FROM f_mongo_test
+  WHERE a >= 1 AND b LIKE '%O%'
+  ORDER BY a;
+
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2, c5 FROM f_test_tbl1 e
+  WHERE c5 <= '1980-12-17' AND c2 IN ('EMP1', 'EMP5', 'EMP10') AND c1 = 100
+  ORDER BY c1;
+SELECT c1, c2, c5 FROM f_test_tbl1 e
+  WHERE c5 <= '1980-12-17' AND c2 IN ('EMP1', 'EMP5', 'EMP10') AND c1 = 100
+  ORDER BY c1;
+
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE c2 = 'EMP10';
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE c2 = 'EMP10';
+
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE c2 < 'EMP10';
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE c2 < 'EMP10';
+
+-- Should not push down if two columns of same table is
+-- involved in single WHERE clause operator expression.
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c6 FROM f_test_tbl1
+  WHERE c1 = c6 AND c1 = 1100
+  ORDER BY c1;
+SELECT c1, c6 FROM f_test_tbl1
+  WHERE c1 = c6 AND c1 = 1100
+  ORDER BY c1;
+
+-- Nested operator expression in WHERE clause. Shouldn't push down.
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE (c1 > 1000) > FALSE;
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE (c1 > 1000) > FALSE;
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE (c1 > 1000) > 0::BOOLEAN;
+SELECT c1, c2 FROM f_test_tbl1
+  WHERE (c1 > 1000) > 0::BOOLEAN;
+
+-- Shouldn't push down operators where the constant is an array.
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT name, marks FROM f_test_tbl3
+  WHERE marks = ARRAY[23::FLOAT, 24::FLOAT]
+  ORDER BY name;
+SELECT name, marks FROM f_test_tbl3
+  WHERE marks = ARRAY[23::FLOAT, 24::FLOAT]
+  ORDER BY name;
+
+-- Pushdown in prepared statement.
 PREPARE pre_stmt_f_mongo_test(int) AS
   SELECT b FROM f_mongo_test WHERE a = $1 ORDER BY b;
 EXPLAIN (VERBOSE, COSTS FALSE)
@@ -118,6 +188,7 @@ DELETE FROM f_mongo_test WHERE a != 0;
 DROP FOREIGN TABLE f_mongo_test;
 DROP FOREIGN TABLE f_test_tbl1;
 DROP FOREIGN TABLE f_test_tbl2;
+DROP FOREIGN TABLE f_test_tbl3;
 DROP USER MAPPING FOR public SERVER mongo_server;
 DROP SERVER mongo_server;
 DROP EXTENSION mongo_fdw;

mongo_fdw.h
@@ -285,6 +285,18 @@ typedef struct ColumnMapping
 	Oid			columnArrayTypeId;
 } ColumnMapping;
 
+/*
+ * FDW-specific planner information kept in RelOptInfo.fdw_private for a
+ * mongo_fdw foreign table.  For a baserel, this struct is created by
+ * MongoGetForeignRelSize.
+ */
+typedef struct MongoFdwRelationInfo
+{
+	/* baserestrictinfo clauses, broken down into safe and unsafe subsets. */
+	List	   *local_conds;
+	List	   *remote_conds;
+} MongoFdwRelationInfo;
+
 /* options.c */
 extern MongoFdwOptions *mongo_get_options(Oid foreignTableId);
 extern void mongo_free_options(MongoFdwOptions *options);
@@ -299,11 +311,12 @@ extern void mongo_cleanup_connection(void);
 extern void mongo_release_connection(MONGO_CONN *conn);
 
 /* Function declarations related to creating the mongo query */
-extern List *ApplicableOpExpressionList(RelOptInfo *baserel);
 extern BSON *QueryDocument(Oid relationId,
 						   List *opExpressionList,
 						   ForeignScanState *scanStateNode);
 extern List *ColumnList(RelOptInfo *baserel);
+extern bool mongo_is_foreign_expr(PlannerInfo *root, RelOptInfo *baserel,
+								  Expr *expression);
 
 /* Function declarations for foreign data wrapper */
 extern Datum mongo_fdw_handler(PG_FUNCTION_ARGS);

commit 1abfac7d82c2e8899da6bd2ac2e3d64e6ac42403

Push only OpExpr clauses to the remote server.
Earlier we were doing the same, but commit
fce56c68aaa03f6aa512f2466b25bdf92a72147d broke that.  Fix it.

FDW-297, Vaibhav Dalvi, reviewed by Jeevan Chalke.

data/mongo_test_data.js
@@ -38,6 +38,6 @@ db.test_tbl1.insertMany([
    {c1: NumberInt(1400), c2 : "EMP14", c3 :"ADMIN", c4 :NumberInt(700) ,c5 :ISODate("1982-01-23"), c6 :1300, c7 :NumberInt(0), c8 :NumberInt(10)  },
 ]);
 db.test_tbl3.insertMany([
-   {name: "dvd", marks: [23, 24]},
-   {name: "vdd", marks: [29, 31]}
+   {name: "dvd", marks: [23, 24], pass: false},
+   {name: "vdd", marks: [29, 31], pass: true}
 ]);

expected/pushdown.out
@@ -17,7 +17,7 @@ CREATE FOREIGN TABLE f_test_tbl1 (_id name, c1 INTEGER, c2 VARCHAR(10), c3 CHAR(
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl1');
 CREATE FOREIGN TABLE f_test_tbl2 (_id name, c1 INTEGER, c2 VARCHAR(14), c3 VARCHAR(13))
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl2');
-CREATE FOREIGN TABLE f_test_tbl3 (_id name, name TEXT, marks FLOAT ARRAY)
+CREATE FOREIGN TABLE f_test_tbl3 (_id name, name TEXT, marks FLOAT ARRAY, pass BOOLEAN)
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl3');
 -- Inserts some values in mongo_test collection.
 INSERT INTO f_mongo_test VALUES ('0', 1, 'One');
@@ -501,6 +501,30 @@ EXECUTE pre_stmt_f_mongo_test(2);
   b  
 -----
  Two
+(1 row)
+
+-- FDW-297: Only operator expressions should be pushed down in WHERE clause.
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT name, marks FROM f_test_tbl3
+  WHERE pass = true
+  ORDER BY name;
+                       QUERY PLAN                       
+--------------------------------------------------------
+ Sort
+   Output: name, marks
+   Sort Key: f_test_tbl3.name
+   ->  Foreign Scan on public.f_test_tbl3
+         Output: name, marks
+         Filter: f_test_tbl3.pass
+         Foreign Namespace: mongo_fdw_regress.test_tbl3
+(7 rows)
+
+SELECT name, marks FROM f_test_tbl3
+  WHERE pass = true
+  ORDER BY name;
+ name |  marks  
+------+---------
+ vdd  | {29,31}
 (1 row)
 
 -- Cleanup

mongo_fdw.c
@@ -259,13 +259,15 @@ MongoGetForeignRelSize(PlannerInfo *root,
 
 	/*
 	 * Identify which baserestrictinfo clauses can be sent to the remote
-	 * server and which can't.
+	 * server and which can't.  Only the OpExpr clauses are sent to the remote
+	 * server.
 	 */
 	foreach(lc, baserel->baserestrictinfo)
 	{
 		RestrictInfo *ri = (RestrictInfo *) lfirst(lc);
 
-		if (mongo_is_foreign_expr(root, baserel, ri->clause))
+		if (IsA(ri->clause, OpExpr) &&
+			mongo_is_foreign_expr(root, baserel, ri->clause))
 			fpinfo->remote_conds = lappend(fpinfo->remote_conds, ri);
 		else
 			fpinfo->local_conds = lappend(fpinfo->local_conds, ri);
@@ -446,7 +448,8 @@ MongoGetForeignPlan(PlannerInfo *root,
 	 * and those that can't.  baserestrictinfo clauses that were previously
 	 * determined to be safe or unsafe are shown in fpinfo->remote_conds and
 	 * fpinfo->local_conds.  Anything else in the restrictionClauses list will
-	 * be a join clause, which we have to check for remote-safety.
+	 * be a join clause, which we have to check for remote-safety.  Only the
+	 * OpExpr clauses are sent to the remote server.
 	 */
 	foreach(lc, restrictionClauses)
 	{
@@ -462,7 +465,8 @@ MongoGetForeignPlan(PlannerInfo *root,
 			remote_exprs = lappend(remote_exprs, rinfo->clause);
 		else if (list_member_ptr(fpinfo->local_conds, rinfo))
 			local_exprs = lappend(local_exprs, rinfo->clause);
-		else if (mongo_is_foreign_expr(root, foreignrel, rinfo->clause))
+		else if (IsA(rinfo->clause, OpExpr) &&
+				 mongo_is_foreign_expr(root, foreignrel, rinfo->clause))
 			remote_exprs = lappend(remote_exprs, rinfo->clause);
 		else
 			local_exprs = lappend(local_exprs, rinfo->clause);

sql/pushdown.sql
@@ -20,7 +20,7 @@ CREATE FOREIGN TABLE f_test_tbl1 (_id name, c1 INTEGER, c2 VARCHAR(10), c3 CHAR(
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl1');
 CREATE FOREIGN TABLE f_test_tbl2 (_id name, c1 INTEGER, c2 VARCHAR(14), c3 VARCHAR(13))
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl2');
-CREATE FOREIGN TABLE f_test_tbl3 (_id name, name TEXT, marks FLOAT ARRAY)
+CREATE FOREIGN TABLE f_test_tbl3 (_id name, name TEXT, marks FLOAT ARRAY, pass BOOLEAN)
   SERVER mongo_server OPTIONS (database 'mongo_fdw_regress', collection 'test_tbl3');
 
 -- Inserts some values in mongo_test collection.
@@ -182,6 +182,15 @@ EXECUTE pre_stmt_f_mongo_test(1);
 EXPLAIN (VERBOSE, COSTS FALSE)
 EXECUTE pre_stmt_f_mongo_test(2);
 EXECUTE pre_stmt_f_mongo_test(2);
+
+-- FDW-297: Only operator expressions should be pushed down in WHERE clause.
+EXPLAIN (VERBOSE, COSTS FALSE)
+SELECT name, marks FROM f_test_tbl3
+  WHERE pass = true
+  ORDER BY name;
+SELECT name, marks FROM f_test_tbl3
+  WHERE pass = true
+  ORDER BY name;
 
 -- Cleanup
 DELETE FROM f_mongo_test WHERE a != 0;